cbdc1594a0c846a5124048cfb74dbfc598521ecf
ParvathyAjRaj <paruvathy99@gmail.com>
2024-02-02 19:23:04 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,78 +1,77 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Kids from './pages/Kids';
-import Ladies from './pages/Ladies';
-import Men from './pages/Men';
+import { useState } from "react";
 import products from "./data";
-import { useState } from 'react';
-import Cart from './pages/hello';
+import Shoes from "./pages/Shoes";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./sidebar/Sidebar";
 import Card from './pages/Card';
+import "./index.css";
 
 function App() {
-  /*-----------------------------Search-------------------------------*/
-  const [userSearch,setUserSearch] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-  function handleSearch(event){
-    setUserSearch(event.target.value);
-  }
+  // ----------- Input Filter -----------
+  const [userInput, setUserInput] = useState("");
 
-  const filteredItem = products.filter((product) => 
-                product.category.toLowerCase().indexOf(userSearch.toLowerCase()) !== -1);
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
 
-  /*---------------------------Radio Filter---------------------------*/
-  const [userFilter,setUserFilter] = useState(null);
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(userInput.toLowerCase()) !== -1
+  );
 
-  function handleFilter(event){
-    setUserFilter(event.target.value);
-  }
+  // ----------- Radio Filtering -----------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
-  /*------------------------------function---------------------------*/
-  function filteredData(products,userSearch,userFilter){
-    let displayData = products;
+ 
 
-    // Search
-    if(userSearch){
-      displayData = filteredItem;
-    }
-    
-    // Filter
-    if(userFilter){
-      displayData = displayData.filter(({category}) => category === userFilter)
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
     }
 
-    // result 
-    return displayData.map(
-      ({ img, title, star, reviews, prevPrice, newPrice }) => 
-        (<Card title={title}/>)
-      //   <Card
-      //     key={Math.random()}
-      //     img={img}
-      //     title={title}
-      //     star={star}
-      //     reviews={reviews}
-      //     prevPrice={prevPrice}
-      //     newPrice={newPrice}
-      //   />
-      // )
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
     );
   }
 
-  const result = filteredData(products,userSearch,userFilter);
+  const result = filteredData(products, selectedCategory, userInput);
 
- 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout 
-                                  handleSearch={handleSearch}
-                                  handleFilter={handleFilter}
-                                  result = {result}/>}>
-        </Route>
-        <Route path='/cart' element={<Cart/>}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+        <Header/>
+        <Sidebar handleFilter = {handleChange}/>
+        <Shoes result = {result} handleSearch={handleInputChange}/>
+        <Footer/>
+    </>
   );
 }
 
